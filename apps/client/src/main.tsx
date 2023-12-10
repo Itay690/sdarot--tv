cc39886f0c7bf137e6e5579d5b009a8689c366ce
Itay690 <153420441+Itay690@users.noreply.github.com>
2023-12-10 12:18:38 +0200

    Create Root Route And Navbar (#2)
    Co-authored-by: Jeremy Toledano <jer0529465704@gmail.com>

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Root } from './routes/root';
 
-import { App } from './app/app';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [{ path: '/hello', element: <div>hello</div> }],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
